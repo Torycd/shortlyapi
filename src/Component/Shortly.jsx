153eb0c890276c4bc07d8672a8b3c200ace2c17e
Torycd <vicidundun@gmail.com>
2024-03-01 18:38:11 +0100

    jj

--- src/Component/Shortly.jsx
+++ src/Component/Shortly.jsx
@@ -2,7 +2,7 @@ import Button from "../UI/Button"
 
 const Shortly = ({className}) => {
   return (
-    <div className="-mt-[40px]">
+    <div className="">
         <div className="mx-16 md:mx-32 px-8 py-6 bg-[#421E47] rounded-lg z-20 -px-[50px]">
             <form className="flex flex-col md:flex-row md:gap-5">
                 <input type="text" className="w-full rounded-lg text-xl px-6" required placeholder="Shortnen a link here..."/>
